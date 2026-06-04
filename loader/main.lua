c8304f5386402bd785806f216b7d3597252eb5fe
npdk1 <huysieu1302@gmail.com>
2026-06-05 01:01:19 +0700

    Add hyperspeedrunner script

--- loader/main.lua
+++ loader/main.lua
@@ -8,4 +8,6 @@ elseif gameID == 122902171482835 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/%2B1heatlhperclick.lua"))()
 elseif gameID == 130526979296684 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/%2B1AgePerClick.lua"))()
+elseif gameID == 88933961678687 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/hyperspeedrunner.lua"))()
 end
