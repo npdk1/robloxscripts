126d8f3c3232cacdc2ea0bda1f28c126cac8a713
npdk1 <huysieu1302@gmail.com>
2026-06-04 21:32:42 +0700

    Update main.lua

--- loader/main.lua
+++ loader/main.lua
@@ -1,1 +1,15 @@
+-- Main Loader for GAMES || npdk with love <33 -- 
+
+local gameID = game.PlaceId
+
+if gameID == 113918641206373 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/nukeyourcity.lua"))()
+elseif gameID == 122902171482835 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/%2B1heatlhperclick.lua"))()
+end
+
+
+
+
+
 
