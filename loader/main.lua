22f3b7402001cc18f4fd419eb530a29dc4f96e86
npdk1 <huysieu1302@gmail.com>
2026-06-06 10:29:55 +0700

    Update main.lua

--- loader/main.lua
+++ loader/main.lua
@@ -1,13 +1,13 @@
--- Main Loader for GAMES -- 
-
 local gameID = game.PlaceId
 
-if gameID == 113918641206373 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/nukeyourcity.lua"))()
-elseif gameID == 122902171482835 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/%2B1heatlhperclick.lua"))()
-elseif gameID == 130526979296684 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/%2B1AgePerClick.lua"))()
-elseif gameID == 88933961678687 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/hyperspeedrunner.lua"))()
+local Scripts = {
+    [113918641206373] = "nukeyourcity",
+    [122902171482835] = "%2B1heatlhperclick",
+    [130526979296684] = "%2B1AgePerClick",
+    [88933961678687] = "hyperspeedrunner",
+    [91695551099066] = "%2B1shrinkperstep",
+}
+
+if Scripts[gameID] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/robloxscripts/refs/heads/main/scripts/" .. Scripts[gameID] .. ".lua"))()
 end
